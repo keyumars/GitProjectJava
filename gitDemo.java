01a570a3d7e139e5fb4e9219c933385ad1819cdd
Mohammad Mobin Karimi <50057451+mmobink@users.noreply.github.com>
2020-04-27 13:09:58 -0700

    add java file commit

---
+++ gitDemo.java
@@ -0,0 +1,11 @@
+package com.selenium.git.project.gitproject;
+
+public class gitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git repository.");
+
+	}
+
+}
